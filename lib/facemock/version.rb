6018e94ae5d13bc33d3146ebde10d8c850cc7d72
ogawatti <ogawattim@gmail.com>
2014-09-03 09:04:58 +0900

    :arrow_up: Upgrade version to 0.0.7

--- lib/facemock/version.rb
+++ lib/facemock/version.rb
@@ -1,3 +1,3 @@
 module Facemock
-  VERSION = '0.0.6'
+  VERSION = '0.0.7'
 end
--- spec/facemock_spec.rb
+++ spec/facemock_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Facemock do
-  let(:version) { '0.0.6' }
+  let(:version) { '0.0.7' }
   let(:db_name) { '.test' }
 
   describe 'VERSION' do
