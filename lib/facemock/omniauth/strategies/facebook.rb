9000843d898782fcacea2dd2fd6e1544134023cd
ogawatti <ogawattim@gmail.com>
2014-10-06 12:33:39 +0900

    Added sample code for omniauth-facebook

--- lib/facemock/omniauth/strategies/facebook.rb
+++ lib/facemock/omniauth/strategies/facebook.rb
@@ -21,7 +21,22 @@ module Facemock
 
         # TODO
         def raw_info
-          super
+          user = Facemock::User.find_by_access_token(access_token.token)
+          @raw_info ||= {
+            :id => user.id.to_s,
+            :name => user.name,
+            #:first_name => 'Joe',
+            #:last_name => 'Bloggs',
+            #:link => 'http://www.facebook.com/jbloggs',
+            #:username => 'jbloggs',
+            #:location => { :id => '123456789', :name => 'Palo Alto, California' },
+            #:gender => 'male',
+            :email => user.email,
+            #:timezone => -8,
+            #:locale => 'en_US',
+            #:verified => true,
+            #:updated_time => '2011-11-11T06:21:03+0000'
+          }
         end
 
         protected
@@ -30,17 +45,25 @@ module Facemock
         def build_access_token
           code = request.params['code']
           authorization_code = Facemock::AuthorizationCode.find_by_string(code)
-          # oauth2-1.0.0/lib/oauth2/client.rb:L139 参照
-          #   raise => fail(Error.new(response))
-          raise unless authorization_code
+          raise_oauth2_error unless authorization_code
           user = Facemock::User.find_by_id(authorization_code.user_id)
-          raise unless user
+          raise_oauth2_error unless user
           app = Facemock::Application.find_by_id(user.application_id)
-          raise unless app
+          raise_oauth2_error unless app
 
           client = OAuth2::Client.new(app.id, app.secret)
           expires = (Time.now + 60*60*24*80).to_i.to_s  # 80 days
           OAuth2::AccessToken.new(client, user.access_token, {"expires" => expires})
+        end
+
+        private
+
+        def raise_oauth2_error
+          response = OAuth2::Response.new(Faraday::Response.new)
+          response.status = 200
+          # ... etc ...
+          error = OAuth2::Error.new(response)
+          fail error  # fail == raise
         end
       end
     end
